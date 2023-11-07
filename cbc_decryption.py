ea5493babebb30011ee41281b93c8bfe29ed7d10
Tjrunner1 <115499975+Tjrunner1@users.noreply.github.com>
2023-11-07 14:45:06 -0500

    Changes thus far

--- cbc_decryption.py
+++ cbc_decryption.py
@@ -1,13 +1,31 @@
 # We are team 6
 from cryptography.hazmat import *
 
-# Define a function to open the wordsEN.txt and return the next key
-
+# Function that adds padding
+def add_padding(s, length):
+    print(length - len(s))
+    print(len(s))
+    for i in range(length - len(s), length):
+        s += " "
+    return s
+    
 
 # Define a function that takes an output and key and nicely outputs it onto decrypted.txt
 
 
 # Define a main function that loops through the wordsEn.txt and prints to decrypted.txt if the output countains "the" in it
+def main():
+    print("Starting")
+    englishWordsFile = open("wordsEn.txt", "r")
+    # for word in englishWordsFile.readlines():
+    word = englishWordsFile.readline()
+    word = add_padding(word, 16)
+    print(len(word))
+    
+    englishWordsFile.close()
 
+
+if __name__ == "__main__":
+    main()
 
 
