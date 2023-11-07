5d5bb15223a050fa544399ff4061adb312f226e2
Tjrunner1 <115499975+Tjrunner1@users.noreply.github.com>
2023-11-07 14:28:44 -0500

    Initial Stuff

---
+++ cbc_decryption.py
@@ -0,0 +1,13 @@
+# We are team 6
+from cryptography.hazmat import *
+
+# Define a function to open the wordsEN.txt and return the next key
+
+
+# Define a function that takes an output and key and nicely outputs it onto decrypted.txt
+
+
+# Define a main function that loops through the wordsEn.txt and prints to decrypted.txt if the output countains "the" in it
+
+
+
